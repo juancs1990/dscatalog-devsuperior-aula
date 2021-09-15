b57904f94a17929b3e49aa0f0433cd272b172ce9
juancs1990 <juancarlos.s2013@gmail.com>
2021-09-15 20:46:08 -0300

    Category Class

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,72 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+	 
+	private static final long serialVersionUID = 1L;
+	private Long Id;
+	private String Name;
+	
+	public Category () {
+		
+	}
+
+	public Category(Long id, String name) {
+ 		Id = id;
+		Name = name;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (Name == null) {
+			if (other.Name != null)
+				return false;
+		} else if (!Name.equals(other.Name))
+			return false;
+		return true;
+	}
+
+ 
+	
+	
+	
+	
+}
